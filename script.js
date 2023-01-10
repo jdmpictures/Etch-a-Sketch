2f4d3df7a10aaf575a1301b6e365adb0491556db
jdmpictures <74938827+jdmpictures@users.noreply.github.com>
2023-01-10 21:05:46 +0000

    grid function added

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
 
 const gridButton = document.getElementById("grid-button");
-const grid = document.getElementById("grid")
+const grid = document.getElementById("grid");
 
 
+// function to set the size of the grid 
 gridButton.addEventListener('click', () => {
     let gridValue = document.getElementById("grid-slider").value;
     let gridVal = parseInt(gridValue);
+    grid.innerHTML = " ";
     grid.style.gridTemplateColumns = `repeat(${gridVal}, 1fr)`;
     grid.style.gridTemplateRows = `repeat(${gridVal}, 1fr)`;
     for(i = 0; i < gridVal * gridVal; i++){
         let newDiv = document.createElement("div");
         newDiv.style.backgroundColor = "black";
         grid.appendChild(newDiv).className = "gird-item"
-
-
     }
+});
+
+let gridDiv = document.getElementsByClassName("gird-item");
+
+gridDiv.addEventListener("mouseover", () => {
+
 });
