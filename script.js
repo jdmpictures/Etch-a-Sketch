035500ccf9720d023a4a69fc5ce1fdefee51f9b0
jdmpictures <74938827+jdmpictures@users.noreply.github.com>
2023-01-13 22:54:01 +0000

    draw function and color input added

--- script.js
+++ script.js
@@ -1,24 +1,64 @@
 
 const gridButton = document.getElementById("grid-button");
-const grid = document.getElementById("grid");
+const colorButton = document.getElementById("color-button");
+let paintColor = 'black'
 
+let rainbow = ''
+
+
+
+
+
+gridButton.addEventListener('click', createGrid)
 
 // function to set the size of the grid 
-gridButton.addEventListener('click', () => {
-    let gridValue = document.getElementById("grid-slider").value;
-    let gridVal = parseInt(gridValue);
+function createGrid () {
     grid.innerHTML = " ";
-    grid.style.gridTemplateColumns = `repeat(${gridVal}, 1fr)`;
-    grid.style.gridTemplateRows = `repeat(${gridVal}, 1fr)`;
+    let gridValue = document.getElementById("grid-slider").value;
+    let gridVal = parseInt(gridValue);    
+    grid.style.gridTemplateColumns = `repeat(${Math.round(gridVal /2.5)}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${Math.round(gridVal / 4.5)}, 1fr)`;
     for(i = 0; i < gridVal * gridVal; i++){
         let newDiv = document.createElement("div");
-        newDiv.style.backgroundColor = "black";
+        newDiv.addEventListener('mouseover', paint)
+        newDiv.style.backgroundColor = 'white';
+
         grid.appendChild(newDiv).className = "gird-item"
     }
-});
 
-let gridDiv = document.getElementsByClassName("gird-item");
 
-gridDiv.addEventListener("mouseover", () => {
+}
 
-});
+colorButton.addEventListener('click',  ()=>{
+    colorSelector();
+})
+//function to set the colour of the paint brush 
+
+function colorSelector () {
+    let colorValue = document.getElementById('color-slider').value;
+    if(colorValue == 2){
+        paintColor = 'red';
+    } else if (colorValue == 3) {
+        paintColor = 'blue';
+    } else if (colorValue == 4) {
+        rainbow = 'active';
+                    while(rainbow == 'active'){
+            paintColor = "#" + Math.floor(Math.random()*16777215).toString(16).padStart(6, '0').toUpperCase();
+        }
+
+
+    }
+}
+
+function rainbowColor (){
+
+}
+ 
+
+function paint () {
+    this.style.backgroundColor = paintColor;
+}
+
+const randColor = () =>  {
+    return "#" + Math.floor(Math.random()*16777215).toString(16).padStart(6, '0').toUpperCase();
+}
