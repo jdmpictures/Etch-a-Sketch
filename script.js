0136bf50cff1f32bcc999dddfc7989ee59d9de62
jdmpictures <74938827+jdmpictures@users.noreply.github.com>
2023-01-19 20:11:17 +0000

    Added draw function and color selection.

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 
 const gridButton = document.getElementById("grid-button");
 const colorButton = document.getElementById("color-button");
-let paintColor = 'black'
-
-let rainbow = ''
-
-
+let colorValue = document.getElementById('color-slider').value;
+let rainbow = '';
+let paintColor = 'black';
 
 
 
@@ -16,49 +14,41 @@ function createGrid () {
     grid.innerHTML = " ";
     let gridValue = document.getElementById("grid-slider").value;
     let gridVal = parseInt(gridValue);    
-    grid.style.gridTemplateColumns = `repeat(${Math.round(gridVal /2.5)}, 1fr)`;
-    grid.style.gridTemplateRows = `repeat(${Math.round(gridVal / 4.5)}, 1fr)`;
+    grid.style.gridTemplateColumns = `repeat(${Math.round(gridVal / 2)}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${Math.round(gridVal / 4 )}, 1fr)`;
     for(i = 0; i < gridVal * gridVal; i++){
         let newDiv = document.createElement("div");
         newDiv.addEventListener('mouseover', paint)
         newDiv.style.backgroundColor = 'white';
-
         grid.appendChild(newDiv).className = "gird-item"
     }
 
-
 }
 
-colorButton.addEventListener('click',  ()=>{
-    colorSelector();
-})
+colorButton.addEventListener('click', colorSelector);
+
+
 //function to set the colour of the paint brush 
 
 function colorSelector () {
-    let colorValue = document.getElementById('color-slider').value;
-    if(colorValue == 2){
+    let color = document.getElementById('color-slider').value;
+    if(color == 2){
+        rainbow = '';
         paintColor = 'red';
-    } else if (colorValue == 3) {
+    } else if (color == 3) {
         paintColor = 'blue';
-    } else if (colorValue == 4) {
+        rainbow = '';
+    } else if (color == 4) {
         rainbow = 'active';
-                    while(rainbow == 'active'){
-            paintColor = "#" + Math.floor(Math.random()*16777215).toString(16).padStart(6, '0').toUpperCase();
-        }
-
-
+        paint();
     }
 }
 
-function rainbowColor (){
-
-}
- 
-
+//Function that activates 
 function paint () {
+    if (rainbow == 'active'){
+            paintColor = "#" + Math.floor(Math.random()*16777215).toString(16).padStart(6, '0').toUpperCase().toString();
+    }
     this.style.backgroundColor = paintColor;
 }
 
-const randColor = () =>  {
-    return "#" + Math.floor(Math.random()*16777215).toString(16).padStart(6, '0').toUpperCase();
-}
